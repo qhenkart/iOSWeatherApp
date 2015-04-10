6ee90814786fe7989325fd214e4381f11aab95af
qhenkart <qhenkart@gmail.com>
2015-04-10 11:12:36 -0700

    Finished basic iOS weather app with Swift

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var userCity: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,54 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func findWeather(sender: AnyObject) {
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/"+userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-")+"/forecasts/latest")
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                var urlError = false
+                var weather = ""
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray.count > 0 {
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        weather = weatherArray[0] as String
+                        
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "°")
+                        
+                    }else {
+                        urlError = true
+                    }
+                    
+                    
+                } else {
+                    urlError = true
+                }
+                dispatch_async(dispatch_get_main_queue()){
+                    
+                    if urlError == true {
+                        self.showError()
+                    } else {
+                        self.resultLabel.text = weather
+                    }
+                }
+            })
+            
+            task.resume()
+            
+        } else {
+            
+            showError()
+        }
+        
+    }
+    
+    func showError() {
+         resultLabel.text = "Was not able to find weather for " + userCity.text + ". Please try again"
+    }
 }
 
